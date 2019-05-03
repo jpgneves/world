e8446e8dd3dd2c6fc782923267606add9d918d4e
João Neves <sevenjp@gmail.com>
2019-05-03 09:35:24 +0200

    Remove go-tools package

--- nix/default.nix
+++ nix/default.nix
@@ -8,7 +8,6 @@ let
        p4 = callPackage ./pkgs/applications/version-management/p4 {};
        opensnitch = callPackage ./pkgs/applications/networking/opensnitch {};
        bingo = callPackage ./pkgs/development/tools/bingo {};
-       gotools = callPackage ./pkgs/development/tools/go-tools {};
        python27Packages = callPackage ./python-packages.nix {
          pythonPackages = pkgs.python27Packages;
        };
