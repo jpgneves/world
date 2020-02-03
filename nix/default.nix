e8e5a92cb21fafd906b6b7407e02883cdd814b64
Joao Neves <joao.neves@embark-studios.com>
2020-02-03 16:54:08 +0100

    Get rid of opensnitch

--- nix/default.nix
+++ nix/default.nix
@@ -13,7 +13,6 @@ let
        };
        emacs = callPackage ./pkgs/applications/editors/emacs {};
        p4 = callPackage ./pkgs/applications/version-management/p4 {};
-       opensnitch = callPackage ./pkgs/applications/networking/opensnitch {};
        bingo = callPackage ./pkgs/development/tools/bingo {};
        evans = callPackage ./pkgs/development/tools/evans {};
        janet = callPackage ./pkgs/development/interpreters/janet {};
